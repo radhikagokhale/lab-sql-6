6ef90b6285b4438788659f37e07a935ec67f6762
Radhika <radhikargokhale@gmail.com>
2022-04-30 16:57:26 +0200

    SQL lab6 commit

---
+++ solution.sql
@@ -0,0 +1,40 @@
+drop table if exists films_2020;
+CREATE TABLE `films_2020` (
+  `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned NOT NULL,
+  `original_language_id` tinyint(3) unsigned DEFAULT NULL,
+  `rental_duration` int(6),
+  `rental_rate` decimal(4,2),
+  `length` smallint(5) unsigned DEFAULT NULL,
+  `replacement_cost` decimal(5,2) DEFAULT NULL,
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT NULL,
+  PRIMARY KEY (`film_id`),
+  CONSTRAINT FOREIGN KEY (`original_language_id`) REFERENCES `language` (`language_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1003 DEFAULT CHARSET=utf8;
+
+
+-- Step 1
+show variables like 'local_infile';
+set global local_infile = 1;
+-- Step 2
+show variables like 'secure_file_priv'; -- this gives you the path you need to save the .csv to
+-- Step 3
+set sql_safe_updates = 0;
+-- you can also import from a file by right clicking the respective table and selecting Table Data Import Wizard
+ 
+ -- you can also import from a file here
+LOAD DATA INFILE 'C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\films_2020.csv' 
+INTO TABLE sakila.films_2020
+FIELDS TERMINATED BY ',';
+
+update sakila.films_2020 
+set rental_duration = 3 ;
+update sakila.films_2020 
+set rental_rate = 2.99 ;
+update sakila.films_2020
+set replacement_cost = 8.99;
+
+select * from sakila.films_2020
